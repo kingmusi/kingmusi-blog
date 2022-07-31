e1eb87e58bf8c6d1454f7a69be4f0d016ab548c4
kingmusi <543657931@qq.com>
2022-07-31 17:52:21 +0800

    feat: 添加预连接

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from '@vuepress/theme-default'
 import { viteBundler } from '@vuepress/bundler-vite'
 
+import { path as vuepressPath } from '@vuepress/utils'
 import { createPage } from '@vuepress/core'
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 import { pwaPlugin } from '@vuepress/plugin-pwa'
@@ -28,6 +29,7 @@ export default defineUserConfig({
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/pwa/favicon-16x16.png' }],
     ['link', { rel: 'shortcut icon', href: '/pwa/favicon.ico' }],
     ['meta', { rel: 'theme-color', content: '#ffffff' }],
+    ['link', { rel: 'preconnect', href: 'https://www.googletagmanager.com' }], // googleAnalytics预连接
     ['meta', { rel: 'msapplication-config', content: '/pwa/browserconfig.xml' }], // pwa
     ['meta', { name: 'baidu-site-verification', content: 'code-JfM5X4P2zS' }], // 百度收录验证
     ['meta', { name: '360-site-verification', content: 'bbeb6891b4135a6e65877a7e7d4be2cd' }] // 360 收录验证
@@ -103,7 +105,7 @@ export default defineUserConfig({
     }
   },
   // 客户端配置
-  clientConfigFile: path.join(__dirname, 'client-config.ts'),
+  clientConfigFile: vuepressPath.resolve(__dirname, 'client-config.ts'),
   // vite 配置
   bundler: viteBundler({
     viteOptions: {
